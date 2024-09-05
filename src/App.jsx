92eb8cebd4b704def284b839ba8a733c45f759f5
GioDP18 <giolagariza@gmail.com>
2024-09-05 11:42:12 +0800

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+
+import Header from "./Layouts/Header/Header"
+
+const App = () => {
+  return (
+    <div className="max-w-[1500px] mx-auto">
+      <Header />
+    </div>
+  )
+}
+
+export default App

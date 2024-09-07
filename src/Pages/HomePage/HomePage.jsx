9b21d9b66b389f4a436f085938e406e8f98b4e68
GioDP18 <giolagariza@gmail.com>
2024-09-07 10:32:11 +0800

    Added location and 404

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -2,12 +2,14 @@
 
 import Banner from "./Components/Banner/Banner"
 import Introduction from "./Components/Introduction/Introduction"
+import Location from "./Components/Location/Location"
 
 const HomePage = () => {
   return (
     <div>
       <Banner />
       <Introduction />
+      <Location />
     </div>
   )
 }
--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -2,12 +2,13 @@ import { createBrowserRouter } from 'react-router-dom';
 import App from '../App.jsx';
 
 import HomePage from '../Pages/HomePage/HomePage.jsx';
+import NotFound from '../NotFound.jsx';
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <App /> ,
-        errorElement: <h1>404 Not Found</h1>,
+        errorElement: <NotFound />,
         children: [
             { path: '', element: <HomePage /> },
             { path: 'about', element: <h1>About</h1> },

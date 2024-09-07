1f056ec148dd8b531c70ad502f46574a59867c69
GioDP18 <giolagariza@gmail.com>
2024-09-07 12:53:05 +0800

    Added framer motion

--- src/Pages/HomePage/Components/Introduction/Introduction.jsx
+++ src/Pages/HomePage/Components/Introduction/Introduction.jsx
@@ -1,32 +1,41 @@
 // import React from 'react'
 import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/react/20/solid'
+import { motion } from 'framer-motion'
+import { fadeIn } from '../../../../variants'
 
 const Introduction = () => {
+  const animateOnce = true
 
-    const features = [
-        {
-            name: 'Push to deploy.',
-            description:
-            'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-            icon: CloudArrowUpIcon,
-        },
-        {
-            name: 'SSL certificates.',
-            description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
-            icon: LockClosedIcon,
-        },
-        {
-            name: 'Database backups.',
-            description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
-            icon: ServerIcon,
-        },
-    ]
+  const features = [
+      {
+          name: 'Push to deploy.',
+          description:
+          'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+          icon: CloudArrowUpIcon,
+      },
+      {
+          name: 'SSL certificates.',
+          description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
+          icon: LockClosedIcon,
+      },
+      {
+          name: 'Database backups.',
+          description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
+          icon: ServerIcon,
+      },
+  ]
 
   return (
     <div className="overflow-hidden bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-          <div className="lg:pr-8 lg:pt-4">
+          <motion.div 
+            variants={fadeIn("right", 0.1)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{once: animateOnce, amount: 0.7}}
+            className="lg:pr-8 lg:pt-4"
+          >
             <div className="lg:max-w-lg">
               <h2 className="text-base font-semibold leading-7 text-indigo-600">Short Introduction</h2>
               <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">CTU FACULTY CENTRUM</p>
@@ -46,14 +55,23 @@ const Introduction = () => {
                 ))}
               </dl>
             </div>
-          </div>
-          <img
-            alt="Product screenshot"
-            src="/images/home/banner.jpg"
-            width={2432}
-            height={1442}
-            className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
-          />
+          </motion.div>
+          <motion.div 
+            variants={fadeIn("left", 0.1)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{once: animateOnce, amount: 0.7}}
+            className=''
+          >
+            <img
+              alt="Product screenshot"
+              src="/images/home/banner.jpg"
+              width={2432}
+              height={1442}
+              className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
+            />
+          </motion.div>
+          
         </div>
       </div>
     </div>

d1617c09ed6d53cd70c9d08cfbcd7f83d542f465
GioDP18 <giolagariza@gmail.com>
2024-09-07 10:36:15 +0800

    Update

--- src/Pages/HomePage/Components/Location/Location.jsx
+++ src/Pages/HomePage/Components/Location/Location.jsx
@@ -6,7 +6,7 @@ const Location = () => {
       <div className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,theme(colors.indigo.100),white)] opacity-20" />
       <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
       <div className="mx-auto max-w-2xl lg:max-w-4xl">
-        <div className="flex gap-2 justify-center items-center">
+        <div className="flex flex-col md:flex-row gap-2 justify-center items-center">
             <img alt="" src="/flat-icons/location.gif" className="h-12" />
             <div className="text-[35px] font-bold text-[#3949AB]">Where to find us?</div>
         </div>
